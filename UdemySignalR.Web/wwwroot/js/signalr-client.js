80e0671fe6433a86bc44f5da46c952c61f982296
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-16 22:59:06 +0300

    connect to Hub

---
+++ UdemySignalR.Web/wwwroot/js/signalr-client.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    const connection = new signalR.HubConnectionBuilder().withUrl("/examplehub").configureLogging(signalR.LogLevel.Information).build();
+    // ilk bağlantı(protokol) rest mimarisi ile istek atılır.=>get/post/put
+    //ardındaki süreç web socket ile bğlantı sağlanr
+    //signalr desteklemiyorsa Server-Sent Events ya da Long Polling ile çift taraflı bağlantı sağlanır
+
+    function start() {
+        connection.start().then(() => console.log("Hub ile Bağlantı Kuruldu!"));
+    }
+    try {
+        start(); // ilk bağnatuı kurulamadığında düşecek
+    }
+    catch {
+        setTimeout(() => start(),5000) //5 saniye sonra tekrardan start metoduna döncek 
+    }
+})
