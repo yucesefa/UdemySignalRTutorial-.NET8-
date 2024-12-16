e2d0befe9eb0295ad0d20ef2060e0e6b8de556fa
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-16 22:29:51 +0300

    Add project files.

---
+++ UdemySignalR.Web/Hubs/ExampleHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace UdemySignalR.Web.Hubs
+{
+    public class ExampleHub:Hub
+    {
+        public async Task BroadcastMessageAllClient(string message)
+        {
+            //ui tarafından tetiklenecek metod çağrılacak ve javascriptteki bir metodu tetikleyecek
+            await Clients.All.SendAsync("ReceiveMessageForAllClient",message);
+        }
+    }
+}
