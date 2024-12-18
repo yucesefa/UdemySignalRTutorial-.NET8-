9768db830a7da101315c9ed1c288857f20c8b936
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-18 20:02:20 +0300

    onConnected&onDisconnectedAsyncMethod

--- UdemySignalR.Web/Hubs/ExampleTypeSafeHub.cs
+++ UdemySignalR.Web/Hubs/ExampleTypeSafeHub.cs
@@ -4,10 +4,23 @@ namespace UdemySignalR.Web.Hubs
 {
     public class ExampleTypeSafeHub:Hub<IExampleTypeSafeHub>
     {
+        private static int connectedClientCount = 0;
         public async Task BroadcastMessageAllClient(string message)
         {
             await Clients.All.ReceiveMessageForAllClient(message);
 
+        }
+        public async override Task OnConnectedAsync()
+        {
+            connectedClientCount++;
+            await Clients.All.ReceiveConnectedClientCountAllClient(connectedClientCount);
+            await base.OnConnectedAsync();
+        }
+        public async override Task OnDisconnectedAsync(Exception? exception)
+        {
+            connectedClientCount--;
+            await Clients.All.ReceiveConnectedClientCountAllClient(connectedClientCount);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
--- UdemySignalR.Web/wwwroot/js/signalr-client.js
+++ UdemySignalR.Web/wwwroot/js/signalr-client.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
     const broadcastMessageToAllClientHubMethodCall = "BroadcastMessageAllClient";
     const receiveMessageForAllClientMethodCall = "ReceiveMessageForAllClient";
+    const receiveConnectedClientCountAllClient = "ReceiveConnectedClientCountAllClient"
     const connection = new signalR.HubConnectionBuilder().withUrl("/exampletypesafehub").configureLogging(signalR.LogLevel.Information).build();
     // ilk bağlantı(protokol) rest mimarisi ile istek atılır.=>get/post/put
     //ardındaki süreç web socket ile bğlantı sağlanr
@@ -13,9 +14,19 @@
         start(); // ilk bağnatuı kurulamadığında düşecek
     }
     catch {
-        setTimeout(() => start(),5000) //5 saniye sonra tekrardan start metoduna döncek 
+        setTimeout(() => start(), 5000) //5 saniye sonra tekrardan start metoduna döncek 
     }
     connection.on(receiveMessageForAllClientMethodCall, (message) => {
+        console.log("Gelen Mesaj", message);
+    })
+
+    var span_client_count = $("#span-connected-client-count");
+    connection.on(receiveConnectedClientCountAllClient, (count) => {
+        span_client_count.text(count);
+        console.log("connected client count", count);
+    })
+
+    connection.on(receiveConnectedClientCountAllClient, (count) => {
         console.log("Gelen Mesaj", message);
     })
     $("#btn-send-message-all-client").click(function () {
