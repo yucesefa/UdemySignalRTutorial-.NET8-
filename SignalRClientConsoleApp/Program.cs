727f208343798df479fd393c5947aed413f5f8e4
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-24 10:03:16 +0300

    consoleapp signalr client-1

---
+++ SignalRClientConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+using SignalRClientConsoleApp;
+
+Console.WriteLine("SignalR Console Client");
+
+var connection = new HubConnectionBuilder().WithUrl("https://localhost:7160/exampleTypeSafeHub").Build();
+connection.StartAsync().ContinueWith((result) =>
+{
+    Console.WriteLine(result.IsCompletedSuccessfully ? "Connected" : "Connection Failed!");
+});
+
+connection.On<Product>(methodName: "ReceiveTypedMessageForAllClient", handler: (product) =>
+{
+    Console.WriteLine($"Received Message:{product.Id}-{product.Name}-{product.Price}");
+
+});
+Console.ReadKey();
