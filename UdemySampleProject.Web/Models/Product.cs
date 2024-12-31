82f4bf756cd02e0128b2cd4d96c190d7dcdc0109
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-31 23:11:21 +0300

    sample project;identity&productlist

---
+++ UdemySampleProject.Web/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UdemySampleProject.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        [Precision(18,2)]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; } = default!;
+    }
+}
---
+++ UdemySampleProject.Web/Models/ViewModels/SignInViewModel.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemySampleProject.Web.Models.ViewModels
+{
+    public record SignInViewModel([Required]string Email,[Required]string Password);
+    
+}
