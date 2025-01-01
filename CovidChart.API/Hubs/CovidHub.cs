482fd5c0ab58f85e9c004782f9529cb5ae2cbb1a
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-01-01 21:21:16 +0300

    covidHub created

---
+++ CovidChart.API/Hubs/CovidHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CovidChart.API.Hubs
+{
+    public class CovidHub:Hub
+    {
+        public async Task GetCovidList()
+        {
+            await Clients.All.SendAsync("ReceiveCovidList","servisten covid 19 veirlerini al");
+        }
+    }
+}
--- CovidChart.API/Program.cs
+++ CovidChart.API/Program.cs
@@ -1,3 +1,4 @@
+using CovidChart.API.Hubs;
 using CovidChart.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,5 +29,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapHub<CovidHub>("/covidhub");
 app.Run();
