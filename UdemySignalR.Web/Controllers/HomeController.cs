cae9474303f78e73a1806059139af32e34a1a494
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-01-01 22:39:22 +0300

    covid19 chart app

--- UdemySignalR.Web/Controllers/HomeController.cs
+++ UdemySignalR.Web/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace UdemySignalR.Web.Controllers
         {
             return View();
         }
+        public IActionResult Covid19Show()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
